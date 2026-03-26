e937fd92e50ff3fe302470704003d4eb483268de
b <brt.ozgr@hotmail.com>
2026-03-26 11:48:04 +0100

    Create homepage

--- TypeLeague/src/components/homepage.vue
+++ TypeLeague/src/components/homepage.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
+<div>
+  <div class="home-wrapper">
+    <h1>{{ $t('greeting.message') }}</h1>
+    <p>{{ $t('greeting.description') }}</p>
+  </div>
+</div>
 
 </template>
 
