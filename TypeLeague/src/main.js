4d0f3d21cd1da6df41fdc557371c54af4df3f905
b <brt.ozgr@hotmail.com>
2026-04-02 14:30:42 +0200

    Implementierung main.css

--- TypeLeague/src/main.js
+++ TypeLeague/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { i18n } from './i18n/i18n.js';
+import './main.css'
 
 const app = createApp(App)
 
