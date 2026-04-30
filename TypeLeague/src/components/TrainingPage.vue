59b0cfb5ab709cbe77b60482627f81d9f1b479a0
b <brt.ozgr@hotmail.com>
2026-04-30 12:19:46 +0200

    Design und spaeter muss noch bots hier reingefugt werden

--- TypeLeague/src/components/TrainingPage.vue
+++ TypeLeague/src/components/TrainingPage.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-
+import { onMounted, onUnmounted } from "vue";
 import Gameboard from "./gameboard.vue";
+
+onMounted(() => {
+  document.body.style.overflow = 'hidden';
+});
+
+onUnmounted(() => {
+  document.body.style.overflow = 'auto';
+});
 </script>
 
 <template>
-<gameboard
-  gametext="AAAA AAAA aaa aaa aaa aaa"
-></gameboard>
+  <div class="training-wrapper">
+    <gameboard
+        gametext="AAAA AAAA aaa aaa aaa aaa"
+    ></gameboard>
+  </div>
 </template>
 
 <style scoped>
-
+.training-wrapper {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  background: #05080D;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 </style>
