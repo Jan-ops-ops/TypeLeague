ab7bde8ed2fe29eb86d86006ebf9e22382c0a756
b <brt.ozgr@hotmail.com>
2026-05-07 13:50:45 +0200

    Supabase-Client initialisiert

--- TypeLeague/src/supabase.ts
+++ TypeLeague/src/supabase.ts
@@ -5,4 +5,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // Erstellt die Verbindung
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
