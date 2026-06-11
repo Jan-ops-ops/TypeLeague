423970a8d0439fb4488eccaf34416c3f22f18603
b <brt.ozgr@hotmail.com>
2026-06-11 08:03:57 +0200

    Supabase update

--- TypeLeague/src/supabase.ts
+++ TypeLeague/src/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Wir holen die Daten aus der .env Datei
 export const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
 export const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Erstellt die Verbindung
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
